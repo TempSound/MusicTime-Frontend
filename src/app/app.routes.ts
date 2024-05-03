c1be37ee36af6e9ecc3af629c11bec733402e970
Victor Herrera <u20201e188@upc.edu.pe>
2024-05-03 10:16:14 -0500

    Create: Added style and functionality to subscription buttons and aircraft descriptions.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import {HomePageComponent} from "./public/pages/home-page/home-page.component";
 import {AboutPageComponent} from "./public/pages/about-page/about-page.component";
 import {MusicianViewComponent} from "./profiles/pages/musician-view/musician-view.component";
+import {SubscriptionComponent} from "./public/pages/subscription/subscription.component";
 import {Routes} from "@angular/router";
+import {ProfileMusicianComponent} from "./profiles/components/profile-musician/profile-musician.component";
 
 export const routes: Routes = [
   { path: 'home', component: HomePageComponent },
   { path: 'about', component: AboutPageComponent },
   { path: 'musician', component: MusicianViewComponent },
+  { path: 'subscription', component: SubscriptionComponent },
+
+  { path: 'musicians/:id', component: ProfileMusicianComponent },
 
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirige a 'home' cuando la ruta es vacía
 ];

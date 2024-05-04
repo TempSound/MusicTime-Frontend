118c6d3d51928ebf90c1a8c59940d6c0edb5c9e0
Carlos Pingus <132234605+KazutoTR@users.noreply.github.com>
2024-05-04 06:25:12 -0500

    Chore: Added Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ToolbarContentComponent} from "./public/components/toolbar-content/toolbar-content.component";
 import {MusicianContentComponent} from "./profiles/components/musician-content/musician-content.component";
+import {FooterContentComponent} from "./public/components/footer-content/footer-content.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ToolbarContentComponent,MusicianContentComponent,],
+  imports: [RouterOutlet, ToolbarContentComponent, MusicianContentComponent, FooterContentComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

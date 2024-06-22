a3fb791bc8ec900aec9cfabdd15e1975a34579db
Anthony07LG <anthony07lg@gmail.com>
2024-06-22 04:42:43 -0500

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,20 @@ import { Routes } from "@angular/router";
 import { EditCreateMusicianComponent } from "./profiles/components/edit-create-musician/edit-create-musician.component";
 import {SubscriptionComponent} from "./public/pages/subscription/subscription.component";
 import {ProfileMusicianComponent} from "./profiles/components/profile-musician/profile-musician.component";
+import {LoginComponent} from "./profiles/components/login/login.component";
+import {RegisterComponent} from "./profiles/components/register/register.component";
+import {ProfileComponent} from "./profiles/components/profile/profile.component";
+import {RegisterCustomerComponent} from "./profiles/components/register-customer/register-customer.component";
+import { CustomerViewComponent } from './profiles/components/customer-view/customer-view.component';
 
 export const routes: Routes = [
+  { path: ' ', component: HomePageComponent },
   { path: 'home', component: HomePageComponent },
+  { path: 'profile/:id', component: ProfileComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'register-customer', component: RegisterCustomerComponent },
+  { path: 'customer-view', component: CustomerViewComponent },
   { path: 'about', component: AboutPageComponent },
   { path: 'musician', component: MusicianViewComponent },
   { path: 'create-musician', component: EditCreateMusicianComponent },
